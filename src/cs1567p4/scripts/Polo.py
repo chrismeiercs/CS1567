7b712f740f535ad90f11159d6cdd334a9a4d4f01
professir <buckcyoung@gmail.com>
2014-07-19 15:18:40 -0400

    todo polo

--- src/cs1567p4/scripts/Polo.py
+++ src/cs1567p4/scripts/Polo.py
@@ -6,8 +6,74 @@ from nav_msgs.msg import *
 from geometry_msgs.msg import *
 from kobuki_msgs.msg import BumperEvent
 
+#Globals
 constant_command_service = None
 send_command             = rospy.ServiceProxy('constant_command', ConstantCommand)
+'''TODO globals here'''
+
+#Constants
+'''TODO constants here'''
+
+
+#Functions
+''' 
+@description:
+    Linear movement forward or backward for a set duration
+@params: 
+    string  direction : 'forward' or 'backward' 
+    integer duration  : length of time to sleep
+'''
+def go(direction, duration):
+    command = Twist()
+    if   direction == "forward"  :
+        command.linear.x = LINEAR_SPEED
+    elif direction == "backward" :
+        command.linear.x = -LINEAR_SPEED
+    send_command(command)
+    rospy.sleep(duration)
+    stop()
+
+''' 
+@description:
+    Angular movement left or right for a set duration
+@params: 
+    string  direction : 'left' or 'right' 
+    integer duration  : length of time to sleep
+'''
+def turn(direction, duration):
+    command = Twist()
+    if   direction == "left"  :
+        command.angular.z = ANGULAR_SPEED
+    elif direction == "right" :
+        command.angular.z = -ANGULAR_SPEED
+    send_command(command)
+    rospy.sleep(duration)
+    stop()
+
+''' 
+@description:
+    Stop all movement
+'''
+def stop():
+    command           = Twist()
+    command.linear.x  = 0.0
+    command.angular.z = 0.0
+    send_command(command)
+
+'''TODO'''
+def arc(direction, duration):
+	
+'''TODO'''
+def constantGo():
+	
+'''TODO'''
+def callPolo():
+	
+
+'''TODO'''
+def bumperCallback(data):
+	
+
 
 def initialize_commands():
     global constant_command_service
